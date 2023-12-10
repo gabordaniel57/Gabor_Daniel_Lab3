af7498046400e8634f451fec6ceedcbc557379bb
dgabor <daniel.cosmin.gabor@reea.net>
2023-12-10 11:29:12 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Gabor_Daniel_Lab3;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Gabor_Daniel_Lab3
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
